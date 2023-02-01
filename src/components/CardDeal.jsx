30a9767e908dd2815c776aee86e0080de89c9878
heyhiru <hirentimbadiya74@gmail.com>
2023-02-01 23:18:57 +0530

    completed CardDeal component

--- src/components/CardDeal.jsx
+++ src/components/CardDeal.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from "react";
+import { card } from "../assets";
+import styles, { layout } from "../style";
+import Button from "./Button";
 
 const CardDeal = () => {
   return (
-    <div>CardDeal</div>
-  )
-}
+    <section className={`${layout.section}`}>
+      <div className={`${layout.sectionInfo}`}>
+        <h2 className={`${styles.heading2}`}>
+          Find a better card deal
+          <br className="sm:block hidden" />
+          in few easy steps.
+        </h2>
+        <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+          Find your perfect credit card in 3 easy steps with our comparison
+          tool. Compare offers, choose the best fit, and upgrade your finances
+          now.
+        </p>
+        <Button styles="mt-10"/>
+      </div>
+      <div className={`${layout.sectionImg}`}>
+            <img src={card} alt="card" className="w-[100%] h-[100%]" />
+      </div>
+    </section>
+  );
+};
 
-export default CardDeal
+export default CardDeal;

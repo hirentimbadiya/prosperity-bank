5ac4146149d661f9c64232a0ab8c2fe69ff5a3fc
heyhiru <hirentimbadiya74@gmail.com>
2023-02-02 14:13:50 +0530

    completed FeedBackCardc component

--- src/components/FeedBackCard.jsx
+++ src/components/FeedBackCard.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
-
-const FeedBackCard = () => {
+import React from "react";
+import { quotes } from "../assets";
+const FeedBackCard = ({ content, name, title, img }) => {
   return (
-    <div>FeedBackCard</div>
-  )
-}
+    <div
+      className={`flex justify-between flex-col px-10 py-12 
+    rounded-[20px] max-w-[370px] md:mr-10 sm:mr-5 mr-0 my-5 feedback-card card`}
+    >
+      <img
+        src={quotes}
+        alt="quotes"
+        className="w-[42px] h-[27px] object-contain"
+      />
+      <p
+        className="text-white font-poppins font-normal 
+      text-[18px] leading[32px] my-10"
+      >
+        {content}
+      </p>
+      <div className="flex flex-row items-center justify-center">
+        <img src={img} alt="image" className="w-[49px] h-[49px] mr-5" />
+        <div className="flex flex-col">
+          <h4 className="text-white font-poppins font-semibold text-[20px] leading-8">{name}</h4>
+          <p className=" text-dimWhite font-poppins font-normal text-[15px] leading-6 "> {title}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default FeedBackCard
+export default FeedBackCard;

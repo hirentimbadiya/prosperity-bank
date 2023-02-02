682d23fb05e3d36caaf45bd152323ad975bf5efa
heyhiru <hirentimbadiya74@gmail.com>
2023-02-02 14:12:54 +0530

    added google and apple store images

--- src/components/Billing.jsx
+++ src/components/Billing.jsx
@@ -27,8 +27,8 @@ const Billing = () => {
         </p>
 
         <div className="flex flex-row flex-wrap sm:mt-10 mt-6">
-            <img src={apple} alt="apple-store"  className="w-[128px] h-[45px] object-contain mr-5 cursor-pointer"/>
-            <img src={google} alt="google-store" className="w-[128px] h-[45px] object-contain cursor-pointer"/>
+            <img src={apple} alt="apple-store"  className="w-[128px] h-[45px] object-contain mr-5 cursor-pointer store"/>
+            <img src={google} alt="google-store" className="w-[128px] h-[45px] object-contain cursor-pointer store"/>
         </div>
       </div>
     </section>

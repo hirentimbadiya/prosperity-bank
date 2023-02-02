8c7a1de12b70b66a2a324f1b026e2165bf09f9a9
heyhiru <hirentimbadiya74@gmail.com>
2023-02-02 14:23:50 +0530

    added hover to get-started component

--- src/components/GetStarted.jsx
+++ src/components/GetStarted.jsx
@@ -5,7 +5,7 @@ const GetStarted = () => {
   return (
     <div
       className={`${styles.flexCenter} w-[140px] h-[140px] 
-      rounded-full bg-blue-gradient p-[2px] cursor-pointer`}
+      rounded-full bg-blue-gradient p-[2px] cursor-pointer getstarted`}
     >
       <div
         className={`${styles.flexCenter} flex-col bg-primary w-[100%] h-[100%] rounded-full`}
